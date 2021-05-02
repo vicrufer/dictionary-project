f9b9d34e0c7036ec9ad882984f62ed638ef338d7
vicrufer <76908321+vicrufer@users.noreply.github.com>
2021-05-02 21:58:18 +0200

    updated css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import Dictionary from "./Dictionary";
+import background from "./background.jpg";
+
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-        </header>
-        <main>
-          <Dictionary defaultKeyword="sunset" />
-        </main>
-        <footer className="App-footer">
-          <small>Coded by Victoria Ruiz</small>
-        </footer>
+    <div style={{ backgroundImage: `url(${background})` }}>
+      <div className="App">
+        <div className="container">
+          <header className="App-header">
+            Dictionary
+          </header>
+          <main>
+            <Dictionary defaultKeyword="sunset" />
+          </main>
+          <footer className="App-footer">
+            <small>Coded by Victoria Ruiz</small>
+          </footer>
+        </div>
       </div>
     </div>
   );
